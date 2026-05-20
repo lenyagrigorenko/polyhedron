ed196cfeec62ab42317c0d02f2f9f277acca751c
lenya <lxnyax@gmail.com>
2026-05-20 19:57:23 +0300

    добавил фигуру и убрал resizable (на макбуке иначе не отображается окно нормально)

--- common/tk_drawer.py
+++ common/tk_drawer.py
@@ -24,10 +24,11 @@ class TkDrawer:
         self.root = Tk()
         self.root.title("Изображение проекции полиэдра")
         self.root.geometry(f"{SIZE+5}x{SIZE+5}")
-        self.root.resizable(False, False)
         self.root.bind('<Control-c>', quit)
         self.canvas = Canvas(self.root, width=SIZE, height=SIZE)
         self.canvas.pack(padx=5, pady=5)
+        self.root.lift()
+        self.root.update()
 
     # Завершение работы
     def close(self):
